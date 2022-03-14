1a7e7d6f1283e83247d6790acffaa2b1a9e3395a
Michael <43648081+michael@users.noreply.github.com>
2022-03-14 23:34:16 +0000

    Add content for module 5

--- assets/js/modules.js
+++ assets/js/modules.js
@@ -2,10 +2,12 @@ import { module as m1 } from './modules1.js';
 import { module as m2 } from './modules2.js';
 import { module as m3 } from './modules3.js';
 import { module as m4 } from './modules4.js';
+import { module as m5 } from './modules5.js';
 
 export const modules = [
   m1,
   m2,
   m3,
-  m4
+  m4,
+  m5
 ]
