f65beda9bc7978ad4ecbe5ed46f6c0c2215a320b
michael <micjustus@gmail.com>
2022-06-17 10:48:09 +0100

    Module 6 structure

--- assets/js/modules.js
+++ assets/js/modules.js
@@ -3,11 +3,13 @@ import { module as m2 } from './modules2.js';
 import { module as m3 } from './modules3.js';
 import { module as m4 } from './modules4.js';
 import { module as m5 } from './modules5.js';
+import { module as m6 } from './modules6.js';
 
 export const modules = [
   m1,
   m2,
   m3,
   m4,
-  m5
+  m5,
+  m6
 ]
---
+++ assets/js/modules6.js
@@ -0,0 +1,341 @@
+export const module = {
+    id: 6,
+    title: "Research Methods and Professional Practice",
+    outcomes: [
+        "Appraise the professional, legal, social, cultural and ethical issues that affect computing professionals.",
+        "Appraise the principles of academic investigation, applying them to a research topic in the applicable computing field.",
+        "Evaluate critically existing literature, research design and methodology for the chosen topic, including data analysis processes.",
+        "Produce and evaluate critically a research proposal for the chosen topic."
+    ],
+    project: "",
+    reflections: "",
+    actionPlan: "",//../assets/artefacts/module5/M6-Action_Plan.pdf",
+    skillsMatrix: "",//../assets/artefacts/module5/M6-Professional_Skills_Matrix.pdf",
+    meetings: [
+      
+    ],
+    units: [
+      {
+        title: "Unit 1",
+        description: "Scientific Investigation and Ethics",
+        outcomes: [
+            "Differentiate between inductive and deductive reasoning.",
+            "Understand why ethics are important and how they may relate to your area of research and your professional practice."        
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U1.pdf",
+        reading: {
+          required: [
+            "Dawson, C. (2015) Projects in Computing and Information Systems: A Student's Guide. Harlow: Pearson. Chapter 2 p 24-25., Chapter 2 p 38-40.",
+            "Bott, F. (2014) Professional Issues in Information Technology. London:BCS. Chapters 1 and 11.",
+            "Stahl, B., Timmermans, J. & Mittelstadt, B. (2016) The Ethics of Computing. ACM Computing Surveys 48(4):1-38. DOI: 10.1145/2871196",
+            "Vanclay, F., Baines, J. & Taylor C. (2013) Principles for ethical research involving humans: ethical professional practice in impact assessment Part I. Impact Assessment and Project Appraisal 31(4): 243-253.",
+            "Vanclay, F., Baines, J. & Taylor C. (2013) Principles for ethical research involving humans: ethical professional practice in impact assessment Part II. Impact Assessment and Project Appraisal 31(4): 243-253.",
+            "Mitchell, J. (2018) Ethics vs Morality. BCS.",
+            "BCS The Chartered Institure for IT. 2021 The Code of Conduct.",
+            "Miessler, D. (2020) The Difference between Deductive and Inductive Reasoning.",
+            "ACM (n.d.) Code of Ethics: Case studies.",
+            "QuestionPro. (2021) What is research?",
+            "Nulli, M., Stahl, B., Ten Holter, C. & de Heaver, M. (2018) Responsible research"
+          ],
+          optional: [
+            "Miller, T., Birch, M., Mauthner, M. & Jessop, J. (2012) Ethics in Qualitative Research. 2nd Ed. London: SAGE. Chapters 4,5,6,10 and 11.",
+            "Anderson, H. & Hepburn, B. (2020) 'Scientific Method' in Zalta, E. The Stanford Encyclopedia of Philosophy (Winter 2020 edition). Metaphysics Research Lab, Stanford University.",
+            "Office for Human Research Protections (OHRP). (2018) The Belmont Report.",
+            "Bailey, M. et al. (2013) Applying Ethical Principles to Information and Communication Technology Research: A Companion to the Menlo Report.",
+            "Gov.uk (n.d.) Government guidance on Data Protection.",
+            "Gov.uk (n.d.) Government guidance on intellectual property.",
+            "Legislation.gov.uk (n.d.) UK Data Protection Act 2018.",
+            "British Research Methodology (BRM). (2018) Research Approach.",
+            "Gray, G. (2014) TEDxVictoria 2014: Trust in research -the ethics of knowledge production."
+          ],
+        },
+        activities: [
+          
+        ],
+      },
+      {
+        title: "Unit 2",
+        description:
+          "Research Questions, the Literature Review and the Research Proposal",
+        outcomes: [
+            "Examine the characteristics that make up a suitable research topic.",
+            "Explore rational and creative methods for formulating a research idea.",
+            "Identify means of transforming research ideas into crafted research questions and proposal.",
+            "Conduct a literature search, critique the literature and to present a literature review."
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U2.pdf",
+        activities: [
+          
+        ],
+        reading: {
+          required: [
+            "Dawson, C. (2015) Projects in Computing and Information Systems: A Student's Guide. Harlow: Pearson. Chapter 1 Introduction p 4. Chapter 1 Section 1.2.2 p 6-8. Chapter 2 Section 2.1 p 17-22. Chapter 2 Section 2.2.1 p 22-24. Chapter 3 Section 3.2 p 44-45. Chapter 3 Section 3.3 p 53-59. Chapter 5 p 90-93. Chapter 5 Section 5.2 p 95-104. Chapter 5 Section 5.6 p 108-112. Chapter 5 Section 5.5 p 106-108.",
+            "Healey, M., Matthews, K. & Cook-Sather, A. (2020) Writing about learning and teaching in higher education: Creating and contributing to scholarly conversations across a range of genres. Center for Engaged Learning Open-Access Books, Elon University. Chapter 14.",
+          ],
+          optional: [
+            "Bott, F. (2014) Professional Issues in Information Technology. London:BCS. Chapters 3, 4, 9-15.",
+            "Marble Jar Channel. (2018) How to write a research paper."
+          ],
+        },
+      },
+  
+      {
+        title: "Unit 3",
+        description: "Research Methods",
+        outcomes: [
+            "Understand the different research methods.",
+            "Know which data collection methods are related to each method of research.",
+            "Have some idea which of these would be suited to your area of research."
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U3.pdf",
+        activities: [
+         
+        ],
+        reading: {
+          required: [
+"Dawson, C. (2015) Projects in Computing and Information Systems: A Student's Guide. Harlow: Pearson. Chapter 2 Section 2.3 p 26-27.",
+"British Research Methodology (BRM). (2018) Research Design.",
+"QuestionPro. (2021). What is research?",
+"Sage (2021) Sage Research Methods: Methods Map."
+          ],
+          optional: [
+"Saunders, M., Lewis, P. & Thornhill, A. (2012) Research Methods for Business Students. 6th ed. Pearson Education Limited. Chapter 5 p 172-220.",
+"QuestionPro (2021) Quantitiative research.",
+          ],
+        },
+      },
+  
+      {
+        title: "Unit 4",
+        description: "Case Studies, Focus Groups and Observations",
+        outcomes: [
+            "Understand how to carry out each of these data collection methods.",
+            "Know which method would be suitable, if any, for your investigation.",
+            "Consider the type of data you would obtain.",
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U4.pdf",
+        activities: [
+         
+        ],
+        reading: {
+          required: [
+            "Dawson, C. (2015) Projects in Computing and Information Systems: A Student's Guide. Harlow: Pearson. Chapter 2 Section 2.4.2 p 28. Chapter 2 Section 2.4.5 p 37-38."
+          ],
+          optional: [
+"Farquhar J D. (2012) Case Study Research for Business. Sage. Chapters 1 to 7.",
+"Abramson, C. & Sánchez-Jankowski, M. (2020) Conducting Comparative Participant Observation. in: Abramson, C. & Gong, N. (Eds) Beyond the Case: The Logics and Practices of Comparative Ethnography. Oxford Scholarship Online",
+"Vanclay, F., Baines, J. & Taylor C. (2013) Principles for ethical research involving humans: ethical professional practice in impact assessment Part II. Impact Assessment and Project Appraisal 31(4): 243-253.",
+"Sim, J. & Waterfield, J. (2019) Focus Group Methodology: Some Ethical Challenges. Quality and Quantity 53: 3003–3022.",
+"Hayes, R, Kyer, B. & Weber, E. (2015) The Case Study Cookbook.",
+"Devault, G. (2020) What is a Market Research Focus Group?",
+"Fuelcycle (2019) The Three Most Comon Observation Research Methods."
+          ],
+        },
+      },
+  
+      {
+        title: "Unit 5",
+        description: "Interviews and Survey Methods",
+        outcomes: [
+            "Understand how interviews and surveys fit into research and whether either or both of these might be useful for your investigation.",
+            "Understand how pre- and post-testing can be used in research."
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U5.pdf",
+        reading: {
+          required: [
+            "Dawson, C. (2015) Projects in Computing and Information Systems: A Student's Guide. Harlow: Pearson. Chapter 2 Section 2.4.2 p 29. Chapter 2 Section 2.4.3 p 29-31.",
+            "Berenson, L., Levine, D. & Szabat, K. (2015) Basic Business Statistics: Concepts and Applications. 13th Ed. Pearson  Chapter 7 Section 7.1 p 280-284. Chapter 7 Section 7.2 p 285-287.",
+            "Confessore, N. (2018) Cambridge Analytica and Facebook: The Scandal and the Fallout So Far."
+          ],
+          optional: [
+            "Campos, D. (2015) Populations vs Samples."
+          ],
+        },
+        activities: [
+          
+        ],
+      },
+  
+      {
+        title: "Unit 6",
+        description: "Questionnaire Design",
+        outcomes: [
+            "Tell the difference between a good questionnaire and a poor one.",
+            "Design a questionnaire to obtain the responses you will need for your investigation.",
+            "Understand how the data obtained can be analysed."
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U6.pdf",
+        reading: {
+          required: [
+            "Dawson, C. (2015) Projects in Computing and Information Systems: A Student's Guide. Harlow: Pearson. Chapter 2 Section 2.4.4 p 31-37."
+          ],
+          optional: [
+"Brace, I. (2013) Questionnaire Design: How to Plan, Structure and Write a Survey. Kogan Page. Chapter 1 p 6-16. Chapter 3 p 28-3. Chapter 4 p 36-47.",
+"Sage Video (2018) Designing a Survey.",
+"QuestionPro (2021) Questionnaire vs Survey: Is there a Difference?",
+"Survey Monkey (n.d.) Survey vs Questionnaire: What is the difference?",
+"QuestionPro (2021) Leading Questions: Definitions, Characteristics and Examples."
+          ],
+        },
+        activities: [
+          
+        ],
+      },
+  
+      {
+        title: "Unit 7",
+        description: "Validity and Generalisability in Research",
+        outcomes: [
+            "Understand how the concepts of validity, generalisability and reliability affect your investigation and the design of your research method.",
+            "Consider how to analyse and present the results you obtain from your investigation and how they will enable you to answer your research question."
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U7.pdf",
+        reading: {
+          required: [
+"Kaplan, B. & Maxwell, J.A. (2005) Qualitative Research Methods for Evaluating Computer Information Systems. In: Anderson J.G. & Aydin C.E. (Eds) Evaluating the Organizational Impact of Healthcare Information Systems. Health Informatics. New York, NY: Springer.",
+"Backman, R. (2012) Confounding Variables.",
+          ],
+          optional: [
+          ],
+        },
+        activities: [
+          
+        ],
+      },
+  
+      {
+        title: "Unit 8",
+        description: "Inferential Statistics",
+        outcomes: [
+            "Identify the different levels of measurement.",
+            "Produce measures of location and spread.",
+            "Perform appropriate hypothesis tests."
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U8.pdf",
+        reading: {
+          required: [
+"Berenson, L., Levine, D. & Szabat, K. (2015) Basic Business Statistics: Concepts and Applications. 13th Ed. Pearson. Chapter 1 Section 1.2 p 44-45. Chapter 3 Section 3.1 130-135. Chapter 3 Section 3.2 p 135-144. Chapter 3 Section 3.3 p 148-153." +
+"Chapter 3 Section 3.4 p 155-157. Chapter 3 Section 3.6 p 165-166. Chapter 9 Section 9.1 p 336-348. Chapter 9 Section 9.2 p 348-353." +
+"Chapter 9 Section 9.3 p 356-359. Chapter 9 Section 9.5 p 364-365. Chapter 10 Section 10.1 p 375-384." +
+"Chapter 10 Section 10.2 p 387-394. Chapter 18 Section 18.1 p 735-739. Chapter 1 Section 1.1 p 42-43."
+          ],
+          optional: [],
+        },
+        activities: [
+         
+        ],
+      },
+  
+      {
+        title: "Unit 9",
+        description: "Analysing Qualitative Data",
+        outcomes: [
+            "Understand the different types of analysis and how they may be useful for the data you have collected.",
+            "Understand the different charts available to present the different types of data you have obtained."
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U9.pdf",
+        reading: {
+          required: [
+"Berenson, L., Levine, D. & Szabat, K. (2015) Basic Business Statistics: Concepts and Applications. 13th Ed. Pearson. Chapter 2 Section 2.2 p 66-68. Chapter 2 Section 2.3 p 79-83. Chapter 2 Section 2.4 p 85-90. Chapte 2 Section 2.5 p 93-95.", 
+"Learning for Action (n.d.). Analyzing Qualitative Data.", 
+          ],
+          optional: [
+            
+"Schneider, C. (2018) Making the case: A Qualitative Approach to Studying Social Media Documents in: Bryman, A. & Buchanon D. (Eds) Unconventional Methodology in Organization and Management Research. Oxford Scholarship Online.", 
+"Business Research Methodology (BRM) (2018) Qualitative Data Analysis Methods.", 
+"Bhatia, M. (2018) Your Quide to Qualitative and Quantitative Data Analysis Methods. Humans of Data."
+          ],
+        },
+        activities: [
+         
+        ],
+      },
+  
+      {
+        title: "Unit 10",
+        description: "Research Writing",
+        outcomes: [
+            "Understand how to structure a dissertation and how to prepare yourself for the writing."
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U10.pdf",
+        reading: {
+          required: [
+"Dawson, C. (2015) Projects in Computing and Information Systems: A Student's Guide. Harlow: Pearson. Chapter 8.",
+"Urban, T. (2016) TEDTalk: Inside the mind of a master procrastinator.",
+"University of Essex (2012) How to write a research project.",
+          ],
+          optional: [
+"Heard, S. (2016) The Scientist's Guide to Writing. Princeton, N.J.: Princeton University Press.",
+"Farquhar J D. (2012) Case Study Research for Business. Sage. Chapter 8.",
+"Dawson, C. (2015) Projects in Computing and Information Systems: A Student's Guide. Harlow: Pearson. Chapter 9.",
+"Kitchin, R. & Fuller, D. (2005) The Academic's Guide to Publishing. London: Sage. Chapters 1, 6 and 7."
+          ],
+        },
+        activities: [
+         
+        ],
+      },
+  
+      {
+        title: "Unit 11",
+        description: "Professional Development – Your e-Portfolio",
+        outcomes: [
+            "Provide necessary reflection for the completion your learning loop.",
+            "Complete the Professional Skills matrix and ensuing action plan."
+        ],
+        writing: "",//../assets/artefacts/module5/M5-Reflection_U11.pdf",
+        reading: {
+          required: [
+"Weber, K. (2018) Employer perceptions of an engineering student’s electronic portfolio. International Journal of ePortoflio 8(1): 57–71.",
+"Händel, M., Wimmer, B. & Ziegler, A. (2018) E-portfolio use and its effects on exam performance – a field study. Studies in Higher Education 45(2): 258–270.",
+"Miller, R. & Morgaine, W. (2009) The Benefits of E-portfolios for Students and Faculty in Their Own Words. Peer Review 11(1): 8-12.",
+"Ring, G., Waugaman, C. & Brackett, B. (2017) The Value of Career ePortfolios on Job Applicant Performance: Using Data to Determine Effectiveness. International Journal of ePortfolio 7(2): 225-236.",
+"Winchell, Z. (2018) E-Portfolios and Their Uses in Higher Education.",
+"Androitis, N. (2018).The 6 benefits of ePortfolios and How to Create Them.",
+"UoEO. Short Guide to Reflective Writing.",
+"Quast, L. (2013) How to conduct a personal SWOT Analysis"
+          ],
+          optional: [
+"Jisc (2019) Digital storytelling for the future.",
+"Jisc (2019) Building digital and employability capabilities into the curriculum.",
+"Jisc (2012) Stories of e-Portfolio Implementation - Thanet College, Kent."
+          ],
+        },
+        activities: [
+         
+        ],
+      },
+  
+      {
+        title: "Unit 12",
+        description:
+          "Project Management and Managing Risk",
+        outcomes: [
+            "Define and explain the concept of project management.",
+            "Demonstrate an appreciation of project life cycles and methodologies.",
+            "Appreciate some of the technologies/software needed to support remote collaboration.",
+            "Understand and explain how projects can be impacted by risk and uncertainty.",
+            "Prepare a risk management plan.",
+            "Appreciate how to control risk and manage project change effectively.",
+        ],
+        writing: "", //"../assets/artefacts/module5/M5-Reflection_U12.pdf",
+        reading: {
+          required: [
+"Maylor, H. (2010) Project Management. 4th Ed. Pearson Prentice Hall. Chapters 1, 3 and 10.",
+"Mircea, E. (2019) Project Management using Agile Frameworks. Economy Informatics 19(1): 34-44.",
+"Marichetty, K. (2017) The Use of Effective Risk Management in Cloud Computing Projects. Thesis. Harrisburg University of Science and Technology.",
+"Free Management Library (n.d.). All About Project Management.",
+"CAST (n.d.) Risk Management in Software Development and Software Engineering Projects."
+          ],
+          optional: [
+"Chapman, R. (2019) Exploring the Value of Risk Management for Projects: Improving Capability Through the Deployment of a Maturity Model. IEEE Engineering Management Review 47(1): 126-143.",
+"Successful projects (n.d.). Project Management Methodologies.",
+          ],
+        },
+        activities: [
+          
+        ],
+      },
+    ],
+  };
+  
